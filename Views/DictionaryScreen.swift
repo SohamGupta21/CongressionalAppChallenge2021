b9d0e6a2c78485b30b989fc34e4b51f341cebaa5
Saksham <catchgsaksham@gmail.com>
2021-06-13 15:47:50 -0500

    blah

--- Views/DictionaryScreen.swift
+++ Views/DictionaryScreen.swift
@@ -9,17 +9,12 @@ import SwiftUI
 
 struct DictionaryScreen: View {
     var body: some View {
-        NavigationView{
-            VStack{
-                Text("Dictionary")
-            //            NavigationLink(
-            //                destination: CameraScreen(),
-            //                label: {
-            //                    Text("Home")
-            //            }).navigationTitle("Dictionary")
+        NavigationView {
 
-                TextField("Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
-            }
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+
+                
+            .navigationTitle(Text("SwiftUI"))
         }
         
     }
