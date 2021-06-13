21753a2433b825c51f3e03d9b5c1ff47284938b9
Soham Gupta <guptasoham@outlook.com>
2021-06-13 13:50:07 -0500

    Initial Commit

---
+++ QuickDictionary/QuickDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDictionaryApp.swift
+//  QuickDictionary
+//
+//  Created by soham gupta on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
