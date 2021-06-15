35ab8879dfa8020ea893865440e11ce6ed885777
Soham Gupta <guptasoham@outlook.com>
2021-06-15 18:30:05 -0500

    gradient title

--- Views/CameraScreen.swift
+++ Views/CameraScreen.swift
@@ -11,58 +11,67 @@ import AVFoundation
 struct CameraScreen: View {
     @StateObject var camera = CameraModel()
     var body: some View {
-        ZStack{
-            CameraPreview(camera: camera)
-                .ignoresSafeArea(.all, edges: .all)
-            VStack{
-                if camera.isTaken{
-                    HStack{
-                        Spacer()
-                        Button(action: camera.reTake, label:{
-                            Image(systemName: "arrow.triangle.2.circlepath.camera")
-                                .foregroundColor(.black)
-                                .padding()
-                                .background(Color.white)
-                                .clipShape(Circle())
-                        })
-                        .padding(.trailing)
-                    }
-                    
-                }
-                Spacer()
-                HStack{
+        NavigationView{
+            ZStack{
+                CameraPreview(camera: camera)
+                    .ignoresSafeArea(.all, edges: .all)
+                VStack{
                     if camera.isTaken{
-                        Button(action: {if !camera.isSaved{camera.savePic()}}, label:{
-                            Text(camera.isSaved ? "Saved":"Save")
-                                .foregroundColor(.black)
-                                .fontWeight(.semibold)
-                                .padding(.vertical, 10)
-                                .padding(.horizontal,20)
-                                .background(Color.white)
-                                .clipShape(Capsule())
-                        })
-                        .padding(.leading)
-                        Spacer()
-                    }else{
-                        Button(action:camera.takePic,label:{
-                            ZStack{
-                                Circle()
-                                    .fill(Color.white)
-                                    .frame(width: 65, height: 65)
-                                Circle()
-                                    .stroke(Color.white, lineWidth:2)
-                                    .frame(width:75, height:75)
-                            }
-                        })
+                        HStack{
+                            Spacer()
+                            Button(action: camera.reTake, label:{
+                                Image(systemName: "arrow.triangle.2.circlepath.camera")
+                                    .foregroundColor(.black)
+                                    .padding()
+                                    .background(Color.white)
+                                    .clipShape(Circle())
+                            })
+                            .padding(.trailing)
+                        }
+                        
                     }
-                    
+                    Spacer()
+                    HStack{
+                        if camera.isTaken{
+                            NavigationLink(
+                                destination: DictionaryScreen(),
+                                label: {
+                                    Text("hello")
+//                                    Button(action: {if !camera.isSaved{camera.savePic()}}, label:{
+//                                        Text(camera.isSaved ? "Saved":"Save")
+//                                            .foregroundColor(.black)
+//                                            .fontWeight(.semibold)
+//                                            .padding(.vertical, 10)
+//                                            .padding(.horizontal,20)
+//                                            .background(Color.white)
+//                                            .clipShape(Capsule())
+//                                    })
+//                                    .padding(.leading)
+                                })
+                            Spacer()
+                        }else{
+                            Button(action:camera.takePic,label:{
+                                ZStack{
+                                    Circle()
+                                        .fill(Color.white)
+                                        .frame(width: 65, height: 65)
+                                    Circle()
+                                        .stroke(Color.white, lineWidth:2)
+                                        .frame(width:75, height:75)
+                                }
+                            })
+                        }
+                        
+                    }
+                    .frame(height:75)
                 }
-                .frame(height:75)
             }
+            .onAppear(perform:{
+                camera.Check()
+            })
+            .navigationBarHidden(true)
+            .navigationBarTitle("", displayMode: .inline)
         }
-        .onAppear(perform:{
-            camera.Check()
-        })
     }
 }
 
--- Views/DictionaryScreen.swift
+++ Views/DictionaryScreen.swift
@@ -8,30 +8,41 @@
 import SwiftUI
 
 struct DictionaryScreen: View {
+    
     var body: some View {
-        var cards = [CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button")]
-        VStack{
-            HStack{
-                Image(systemName: "magnifyingglass")
-                TextField("Placeholder", text: .constant(""))
-            }
-            .padding()
-            ScrollView{
-                VStack{
-//                    for card in 0..<cards.count {
-//                        cards[card]
-//                            .on
-//                    }
-                    ForEach(cards.indices, id: \.self){ card in
-                        cards[card]
-                            .onTapGesture {
-//                                cards[card].downvotes = cards[card].downvotes + 1
-                                print("got here")
-                            }
-                    }
+        var cards = [CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button"),CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button"),CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button"),CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button"),CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button"),CardComponent(word: "insanity", sound: "/inˈsanədē/", meaning: "the state of being seriously mentally ill", upvotes: 5, downvotes: 3, partOfSpeech: "noun", image: "swiftui-button")]
+        NavigationView{
+            VStack{
+                HStack{
+                    Text("Quictionary").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).font(.largeTitle).foregroundColor(.clear).padding([.trailing]).overlay(LinearGradient(gradient: Gradient(colors: [Color.red,Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing).mask(Text("Quictionary").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).font(.largeTitle).scaledToFill()))
+                        .padding(.top)
                     Spacer()
+                }.padding(.top)
+                
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                    TextField("Placeholder", text: .constant(""))
+                }
+                .padding()
+                ScrollView{
+                    VStack{
+    //                    for card in 0..<cards.count {
+    //                        cards[card]
+    //                            .on
+    //                    }
+                        ForEach(cards.indices, id: \.self){ card in
+                            cards[card]
+                                .onTapGesture {
+    //                                cards[card].downvotes = cards[card].downvotes + 1
+                                    print("got here")
+                                }
+                        }
+                        Spacer()
+                    }
                 }
             }
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
         }
     }
 }
